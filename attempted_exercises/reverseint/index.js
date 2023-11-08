a7af6d0eabe39320b6c9157ede795deba5ad3937
Stephan Lefort <lefortsge@gmail.com>
2023-11-08 14:07:00 -0800

    attempt reverseint exercise - First Solution

--- attempted_exercises/reverseint/index.js
+++ attempted_exercises/reverseint/index.js
@@ -8,6 +8,17 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+
+  let strN = JSON.stringify(Math.abs(n));
+  let revStrN = strN.split('').reverse();
+  let revN = Number(revStrN.join(''));
+  
+  if (n < 0) {
+    return -revN;
+  }
+
+  return revN;
+}
 
 module.exports = reverseInt;
